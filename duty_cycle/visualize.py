4c335f8ba0e8add6ba1aadb8e3de054100fcc979
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-20 15:25:22 +0100

    Rename simulation_output to bit_ts

--- duty_cycle/visualize.py
+++ duty_cycle/visualize.py
@@ -4,7 +4,7 @@ from matplotlib import pyplot as plt
 from .simulate import _UP, _DOWN
 from .utils import find_contiguous_up_and_down_segments
 
-def visualize_duty_cycle(simulation_output, use_tex=True):
+def visualize_duty_cycle(bit_ts, use_tex=True):
     if use_tex:
         plt.rcParams.update({
             "text.usetex": True,
@@ -17,21 +17,21 @@ def visualize_duty_cycle(simulation_output, use_tex=True):
     fig = plt.figure(dpi=150)
     ax = fig.gca()
 
-    cont_up_segments, cont_down_segments = find_contiguous_up_and_down_segments(simulation_output)
-    N = len(simulation_output)
+    cont_up_segments, cont_down_segments = find_contiguous_up_and_down_segments(bit_ts)
+    N = len(bit_ts)
     
     # Plot the UP segments
     for up_segment in cont_up_segments:
         ax.plot(
             np.arange(up_segment[0], up_segment[1]+1, 1)/N, 
-            simulation_output[up_segment[0]:up_segment[1]+1], 
+            bit_ts[up_segment[0]:up_segment[1]+1], 
             color="tab:green",
         )
     # Plot the DOWN segments
     for down_segment in cont_down_segments:
         ax.plot(
             np.arange(down_segment[0], down_segment[1]+1, 1)/N, 
-            simulation_output[down_segment[0]:down_segment[1]+1], 
+            bit_ts[down_segment[0]:down_segment[1]+1], 
             color="tab:red",
         )
 
