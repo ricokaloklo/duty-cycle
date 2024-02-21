12c10721856736a38519a52e33dc3704a31d5e01
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-21 15:59:11 +0100

    Add docstrings

--- duty_cycle/histogram.py
+++ duty_cycle/histogram.py
@@ -3,10 +3,44 @@ import numpy as np
 from .utils import find_contiguous_up_and_down_segments
 
 def make_histogram_bin_edges(nbin):
+    """
+    Make the bin edges for a histogram of continuous up and down times.
+
+    Parameters
+    ----------
+    nbin : int
+        The number of bins.
+    
+    Returns
+    -------
+    bin_edges : array-like
+        The bin edges.
+    """
     # For now we only allow equal-width bins
     return np.histogram_bin_edges([], bins=nbin)
 
 def make_histograms(data, dt, bin_edges):
+    """
+    Make normalized histograms of continuous up and down times.
+
+    Parameters
+    ----------
+    data : array-like
+        The data to make the histograms from.
+    dt : float
+        The time step.
+    bin_edges : array-like
+        The bin edges.
+    
+    Returns
+    -------
+    binned_cont_up_times : array-like
+        The binned continuous up times. 
+        Normalized so that the sum of the bins is 1.
+    binned_cont_down_times : array-like
+        The binned continuous down times.
+        Normalized so that the sum of the bins is 1.
+    """
     binned_cont_up_times = np.zero(len(bin_edges) - 1)
     binned_cont_down_times = np.zero(len(bin_edges) - 1)
 
