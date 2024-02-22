819bf787749d330ade846e95ff897c6856e417d8
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-22 20:31:33 +0100

    Implement JointIndependentDistribution

--- duty_cycle/prior.py
+++ duty_cycle/prior.py
@@ -1,13 +1,28 @@
 import torch
 from torch import distributions as dist
 
+class JointIndependentDistribution:
+    def __init__(self, *distributions):
+        self.distributions = distributions
+
+    def log_prob(self, x):
+        return sum(d.log_prob(x_i) for d, x_i in zip(self.distributions, x))
+    
+    def sample(self, sample_shape=torch.Size()):
+        return torch.stack([d.sample(sample_shape) for d in self.distributions], dim=-1)
+
 # From https://github.com/pytorch/pytorch/issues/11412
 class LogUniform(dist.TransformedDistribution):
-    def __init__(self, lower_bound, upper_bound):
+    def __init__(self, low, high):
+        if type(low) is not torch.Tensor:
+            low = torch.tensor(low)
+        if type(high) is not torch.Tensor:
+            high = torch.tensor(high)
+
         super(LogUniform, self).__init__(
             dist.Uniform(
-                lower_bound.log(),
-                upper_bound.log()
+                low.log(),
+                high.log()
             ),
             dist.ExpTransform()
         )
