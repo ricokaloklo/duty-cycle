96638e362de936cd98bbb22d1a9adc8c0385c804
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2024-02-29 13:52:21 +0100

    Bump to v0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="duty-cycle",
-    version="0.0.1",
+    version="0.1.0",
     description="Simulate duty cycles of a gravitational-wave detector",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
