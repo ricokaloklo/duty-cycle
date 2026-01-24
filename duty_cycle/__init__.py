6b031d41783729441f063524b079686d85dcb280
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2026-01-24 21:19:05 +0100

    Import some modules in __init__.py

--- duty_cycle/__init__.py
+++ duty_cycle/__init__.py
@@ -1,2 +1,7 @@
 _UP = 1 # Detector is in observation-intent/science mode
 _DOWN = 0 # Detector is NOT in observation-intent/science mode
+
+from . import simulate
+from . import simulate_network
+from . import utils
+from . import infer
