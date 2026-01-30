396808b77b0edf7512f0bc6dd06e2bb5f36e7429
Rico K. L. Lo <rico.kaloklo@gmail.com>
2026-01-30 13:50:04 +0100

    Improve on how we label params on a network level

--- duty_cycle/simulate_network.py
+++ duty_cycle/simulate_network.py
@@ -4,7 +4,16 @@ from . import _UP, _DOWN, _UNDEF
 from .simulate import Simulator
 
 class NetworkSimulator(Simulator):
-    def initialize_network(self, components, sep_char='_'):
+    def register_params(self, simulator_dict):
+        for name, component in simulator_dict.items():
+            for param_name, param_label in zip(component.param_names, component.param_labels):
+                full_param_name = f"{param_name}{self.sep_char}{name}"
+                self.param_names.append(full_param_name)
+                full_param_label = param_label[:-1] + f"^{{\\rm {name}}}" + param_label[-1]
+                self.param_labels.append(full_param_label)
+
+
+    def initialize_network(self, components, sep_char='^'):
         """
         Initialize a network of components for simulation.
 
@@ -13,10 +22,11 @@ class NetworkSimulator(Simulator):
         components : dict
             A dictionary where keys are component names and values are simulator types.
         sep_char : str, optional
-            A separator character used in naming components, by default '_'.
+            A separator character used in naming components, by default '^'.
         """
         self.components = {name: simulator(dt=self.dt, nmax=self.nmax) for name, simulator in components.items()}
         self.sep_char = sep_char
+        self.register_params(self.components)
 
     def initialize_disturbances(self, disturbances):
         """
@@ -24,16 +34,15 @@ class NetworkSimulator(Simulator):
 
         Parameters
         ----------
-        disturbances : list
-            A list of ExternalDisturbance objects.
+        disturbances : dict
+            A dictionary where keys are disturbance names and values are disturbance simulator types.
         """
-        self.disturbances = disturbances
-        # Timeseries flags tracking the state of each disturbance over time
-        # _UP means the disturbance is triggered, _DOWN means it is not triggered
-        self.disturbances_flags = {
-            n : np.ones(self.nmax, dtype=int)*_UNDEF
-            for n in range(len(disturbances))
-        }
+        self.disturbances = {name: simulator(dt=self.dt, nmax=self.nmax) for name, simulator in disturbances.items()}
+        self.register_params(self.disturbances)
+
+    def unpack_params(self, simulation_params, use_torch=True):
+        # Loop over components and disturbances to unpack parameters
+        pass
 
     def simulate_duty_cycle(self, simulation_params):
         """
