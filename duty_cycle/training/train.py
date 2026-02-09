7d93871ee1cd2cc2721f053896b684a98ac3e33f
Ka Lok (Rico) Lo <rico.kaloklo@gmail.com>
2026-02-09 10:32:57 +0100

    Update the example training script

--- duty_cycle/training/train.py
+++ duty_cycle/training/train.py
@@ -5,37 +5,41 @@ from sbi.utils.user_input_checks_utils import (
 )
 from torch.distributions import Uniform
 from duty_cycle.utils import LogUniform
-from duty_cycle.infer import SimulationBasedInference
-from duty_cycle.simulate import SigmoidDropOffVLMC
+from duty_cycle.infer import SimulationBasedInference, SummaryStatisticInference
+from duty_cycle.simulate import WeibullVLMC
+
+model_name = "test-WeibullVLMC-EXP1"
 
 # Set up the prior
-prior_mean_cont_up_time = LogUniform(torch.Tensor([0.05]), torch.Tensor([0.9]))
-prior_std_cont_up_time = LogUniform(torch.Tensor([0.01]), torch.Tensor([0.5]))
-prior_mean_cont_down_time = LogUniform(torch.Tensor([0.05]), torch.Tensor([0.9]))
-prior_std_cont_down_time = LogUniform(torch.Tensor([0.01]), torch.Tensor([0.5]))
-prior_k_cont_up = Uniform(torch.Tensor([1]), torch.Tensor([10]))
-prior_k_cont_down = Uniform(torch.Tensor([1]), torch.Tensor([10]))
-
+"""
+For WeibullVLMC, we have the following parameters:
+['scale_up',
+ 'shape_up',
+ 'initial_deadtime_up',
+ 'scale_down',
+ 'shape_down',
+ 'initial_deadtime_down']
+"""
 prior_for_sbi = MultipleIndependent(
     dists=[
-        prior_mean_cont_up_time,
-        prior_std_cont_up_time,
-        prior_mean_cont_down_time,
-        prior_std_cont_down_time,
-        prior_k_cont_up,
-        prior_k_cont_down,
+        LogUniform(torch.tensor([5e-3]), torch.tensor([1.0])),  # scale_up
+        LogUniform(torch.tensor([1/2.5]), torch.tensor([2.5])),  # shape_up
+        Uniform(torch.tensor([0.0]), torch.tensor([1e-2])),      # initial_deadtime_up
+        LogUniform(torch.tensor([5e-3]), torch.tensor([1.0])), # scale_down
+        LogUniform(torch.tensor([1/2.5]), torch.tensor([2.5])),  # shape_down
+        Uniform(torch.tensor([0.0]), torch.tensor([1e-2])),      # initial_deadtime_down
     ],
 )
 
-inference = SimulationBasedInference(
-    SigmoidDropOffVLMC(N=1000),
+inference = SummaryStatisticInference(
+    WeibullVLMC(nmax=1000, dt=1.0/1000), # For now, set dt = 1/nmax such that the total duration is always 1 time unit
     prior_for_sbi,
     density_estimator="kde",
     ngridpoint=50,
-    grid_range=(1e-2, 1),
+    grid_range=(1e-3, 1),
     grid_spacing="log",
-    nsample=5000,
+    nsample=10000,
 )
 
-inference.train(method="SNPE", nsimulation=10000, ncore=128)
-inference.save_to_file("model.pkl")
+inference.train(method="SNPE", nsimulation=25000, ncore=128)
+inference.save_to_file(f"{model_name}.pkl")
